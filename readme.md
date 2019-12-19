65b9e0f07cefdf8f7150742ca9b2ad7da59fb1c6
HenrikDK2 <Henrik265@outlook.dk>
2019-12-19 18:19:27 +0100

    1.1.0

--- readme.md
+++ readme.md
@@ -9,9 +9,17 @@ npm i @henrikdk/spagettostring
 ```javascript
 const spagettoString = require('@henrikdk/spagettoString');
 
-console.log(spagettoString("Hej", {reverse:true}));
+console.log(spagettoString.strReverse('Hej'));
 //Output: "jeH"
+
+console.log(spagettoString.strSort('Strawberries Raspberries Apple Blueberries Kiwifruit Passionfruit', "asc"));
+//Output: "Apple Blueberries Kiwifruit Passionfruit Raspberries Strawberries"
+
+console.log(spagettoString.strCompare('this apple is an beautifull apple', "apple"));
+//Output: "2"
 ```
 
 ## Functions
-### Reverse - reverse the string.
+### strReverse - reverse the string.
+### strSort - Sort string by ascending or descending.
+### strCompare - Find how many instances of a word exist in a string.
--- src/spagettoString.js
+++ src/spagettoString.js
@@ -1,10 +1,38 @@
-function spagettoString(string, options){
-    if(options.reverse){
+module.exports = {
+    //Reverse string
+    strReverse: function (string) {
         return string.split("").reverse().join("");
-    }else{
-        console.error("ERROR: spagettoString didn't find option!");
-        return string;
+    },
+
+    //Compare every word(string) with find and return the combined amount.
+    strCompare: function (string, find) {
+        let found = 0;
+
+        if(!find){
+            throw "strCompare(string, find) find needs a word to compare with";
+        }
+
+        string.split(" ").forEach(word => {
+            if(word.toLowerCase() === find.toLowerCase()){
+                found++;
+            }
+        });
+
+        return found;
+    },
+
+    //Sort string by alphabetical or descending.
+    strSort: function(string, order){
+        if(!order){
+            throw "strSort(string, order) order needs to be defined";
+        }
+
+        if(order.toLowerCase() === "asc"){
+            return string.split(" ").sort().join(" ");
+        }else if(order.toLowerCase() === "desc"){
+            return string.split(" ").sort().reverse().join(" ");
+        }else{
+            throw "ERROR: You can order by ASC and DESC not "+order;
+        }
     }
 }
-
-module.exports = spagettoString;
